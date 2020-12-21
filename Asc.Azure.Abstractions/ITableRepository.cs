f66c41d99966aed288af51c548c88ea46c0057fb
Alan Castro <alandecastros@gmail.com>
2020-12-21 17:12:22 -0300

    Update ITableRepository.cs

--- Asc.Azure.Abstractions/ITableRepository.cs
+++ Asc.Azure.Abstractions/ITableRepository.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.Azure.Cosmos.Table;
+using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Asc.Azure.Abstractions
 {
     public interface ITableRepository<T> where T : TableEntity, new()
     {
-        Task<T> Get(string partitionKey, string rowKey);
-        Task<IList<T>> GetByPartition(string partitionKey);
-        Task Remove(T entity);
-        Task<T> Save(T entity);
+        Task<T> GetAsync(string rowKey, string partitionKey = null);
+        Task<IEnumerable<T>> GetByPartitionAsync(string partitionKey, CancellationToken ct = default,
+            Action<IList<T>> onProgress = null);
+        Task<IEnumerable<T>> GetByQueryAsync(TableQuery<T> query, CancellationToken ct = default,
+            Action<IList<T>> onProgress = null);
+        Task DeleteAsync(T entity);
+        Task<T> SaveAsync(T entity);
     }
 }
